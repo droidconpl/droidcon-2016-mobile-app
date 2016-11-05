8820e3d99b8300ce268dc4392abe947540fab634
Przemek Jakubczyk <pjakubczyk@gmail.com>
2016-11-05 14:05:57 +0100

    Basic version of speakers list

---
+++ app/src/main/java/pl/droidcon/app/ui/fragment/speaker/SpeakersAdapter.java
@@ -0,0 +1,86 @@
+package pl.droidcon.app.ui.fragment.speaker;
+
+
+import android.support.v7.util.SortedList;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.util.SortedListAdapterCallback;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+import pl.droidcon.app.R;
+import pl.droidcon.app.helper.UrlHelper;
+import pl.droidcon.app.model.db.SpeakerEntity;
+
+public class SpeakersAdapter extends RecyclerView.Adapter<SpeakersAdapter.SpeakerHolder> {
+
+
+    private SortedList<SpeakerEntity> speakers = new SortedList<>(SpeakerEntity.class, new SortedListAdapterCallback<SpeakerEntity>(this) {
+        @Override
+        public int compare(SpeakerEntity o1, SpeakerEntity o2) {
+            return o1.getId() - o2.getId();
+        }
+
+        @Override
+        public boolean areContentsTheSame(SpeakerEntity oldItem, SpeakerEntity newItem) {
+            return oldItem.equals(newItem);
+        }
+
+        @Override
+        public boolean areItemsTheSame(SpeakerEntity item1, SpeakerEntity item2) {
+            return item1.getId() == item2.getId();
+        }
+    });
+
+
+    public void add(SpeakerEntity speakerEntity) {
+        speakers.add(speakerEntity);
+    }
+
+    @Override
+    public SpeakersAdapter.SpeakerHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.agenda_element, parent, false);
+
+        return new SpeakerHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(SpeakersAdapter.SpeakerHolder holder, int position) {
+        holder.bind(speakers.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return speakers.size();
+    }
+
+    static class SpeakerHolder extends RecyclerView.ViewHolder {
+
+        private final Picasso picasso;
+        ImageView speakerPhoto;
+        TextView speakerName;
+
+
+        public SpeakerHolder(View itemView) {
+            super(itemView);
+            speakerPhoto = (ImageView) itemView.findViewById(R.id.session_picture);
+            speakerName = (TextView) itemView.findViewById(R.id.session_title);
+            picasso = Picasso.with(itemView.getContext());
+        }
+
+        public void bind(SpeakerEntity speakerEntity) {
+
+            String url = UrlHelper.url(speakerEntity.getImageUrl());
+
+            picasso.load(url).into(speakerPhoto);
+
+            speakerName.setText(String.format("%s %s", speakerEntity.getFirstName(), speakerEntity.getLastName()));
+
+
+        }
+    }
+}
---
+++ app/src/main/java/pl/droidcon/app/ui/fragment/speaker/SpeakersFragment.java
@@ -0,0 +1,123 @@
+package pl.droidcon.app.ui.fragment.speaker;
+
+import android.graphics.Rect;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import io.requery.query.Result;
+import pl.droidcon.app.R;
+import pl.droidcon.app.dagger.DroidconInjector;
+import pl.droidcon.app.database.DatabaseManager;
+import pl.droidcon.app.model.db.SpeakerEntity;
+import pl.droidcon.app.ui.decoration.SpacesItemDecoration;
+import pl.droidcon.app.ui.fragment.BaseFragment;
+import rx.Observable;
+import rx.Subscriber;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Func1;
+import rx.schedulers.Schedulers;
+
+public class SpeakersFragment extends BaseFragment {
+
+    public static final String TAG = SpeakersFragment.class.getSimpleName();
+
+    public static SpeakersFragment newInstance() {
+        return new SpeakersFragment();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.speakers_fragment, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.speakers_list);
+
+        recyclerView.setLayoutManager(new GridLayoutManager(getContext(), 2));
+
+        final SpeakersAdapter speakersAdapter = new SpeakersAdapter();
+        GridLayoutManager mLayoutManager = new GridLayoutManager(view.getContext(), 2);
+
+        recyclerView.setLayoutManager(mLayoutManager);
+
+        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(getContext(), mLayoutManager.getOrientation());
+//        recyclerView.addItemDecoration(new SpacesItemDecoration(view.getContext().getResources().getDimension(R.dimen.list_element_margin)));
+
+//        recyclerView.addItemDecoration(dividerItemDecoration);
+        recyclerView.addItemDecoration(new SpacesItemDecoration((int) getResources().getDimension(R.dimen.list_element_margin)));
+
+
+        recyclerView.setAdapter(speakersAdapter);
+
+        DatabaseManager store = DroidconInjector.get().databaseManager();
+
+        store
+                .speakers()
+                .flatMap(new Func1<Result<SpeakerEntity>, Observable<SpeakerEntity>>() {
+                    @Override
+                    public Observable<SpeakerEntity> call(Result<SpeakerEntity> speakerEntities) {
+
+                        return speakerEntities.toObservable();
+                    }
+                })
+                .subscribeOn(Schedulers.io())
+                .compose(this.<SpeakerEntity>bindToLifecycle())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Subscriber<SpeakerEntity>() {
+                    @Override
+                    public void onCompleted() {
+
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+
+                    }
+
+                    @Override
+                    public void onNext(SpeakerEntity speakerEntity) {
+                        speakersAdapter.add(speakerEntity);
+                    }
+                });
+
+    }
+
+    @Override
+    public int getTitle() {
+        return R.string.speakers;
+    }
+
+    @Override
+    public String getFragmentTag() {
+        return TAG;
+    }
+
+
+    public class SpacesItemDecoration extends RecyclerView.ItemDecoration {
+        private int space;
+
+        public SpacesItemDecoration(int space) {
+            this.space = space;
+        }
+
+        @Override
+        public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
+            outRect.left = space;
+            outRect.right = space;
+            outRect.bottom = space;
+
+            // Add top margin only for the first item to avoid double space between items
+//            if(parent.getChildPosition(view) == 0)
+                outRect.top = space;
+        }
+    }
+}
