e260c1f5f775e48ae371bed7d3e8e0f0d752a7f5
Przemek Jakubczyk <pjakubczyk@applause.com>
2016-11-11 17:56:50 +0100

    Enable databinding for Schedule Main Fragment

--- app/src/main/java/pl/droidcon/app/ui/fragment/schedule/ScheduleMainFragment.java
+++ app/src/main/java/pl/droidcon/app/ui/fragment/schedule/ScheduleMainFragment.java
@@ -2,21 +2,18 @@ package pl.droidcon.app.ui.fragment.schedule;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import org.joda.time.DateTime;
 
-import butterknife.Bind;
-import butterknife.ButterKnife;
 import pl.droidcon.app.R;
 import pl.droidcon.app.dagger.DroidconInjector;
+import pl.droidcon.app.databinding.FavouritesFragmentBinding;
 import pl.droidcon.app.helper.DateTimePrinter;
 import pl.droidcon.app.model.common.SessionDay;
 import pl.droidcon.app.ui.fragment.BaseFragment;
@@ -24,6 +21,7 @@ import pl.droidcon.app.ui.fragment.BaseFragment;
 public class ScheduleMainFragment extends BaseFragment {
 
     public static final String TAG = ScheduleMainFragment.class.getSimpleName();
+    private FavouritesFragmentBinding binding;
 
     public static ScheduleMainFragment newInstance() {
         Bundle args = new Bundle();
@@ -31,12 +29,6 @@ public class ScheduleMainFragment extends BaseFragment {
         fragment.setArguments(args);
         return fragment;
     }
-
-    @Bind(R.id.tabs)
-    TabLayout tabLayout;
-
-    @Bind(R.id.viewpager)
-    ViewPager viewPager;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -47,15 +39,15 @@ public class ScheduleMainFragment extends BaseFragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.favourites_fragment, container, false);
+        binding = FavouritesFragmentBinding.inflate(inflater, container, false);
+        return binding.getRoot();
     }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        ButterKnife.bind(this, getView());
-        viewPager.setAdapter(new MyScheduleFragmentAdapter(getChildFragmentManager()));
-        tabLayout.setupWithViewPager(viewPager);
+        binding.viewpager.setAdapter(new MyScheduleFragmentAdapter(getChildFragmentManager()));
+        binding.tabs.setupWithViewPager(binding.viewpager);
     }
 
     @Override
