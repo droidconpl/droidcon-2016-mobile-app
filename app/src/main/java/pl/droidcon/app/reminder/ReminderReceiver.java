394b7c25c868ce74fb7b696bf8b1388d4eb912e1
whiter13 <bialkowski.jakub@gmail.com>
2016-11-12 15:06:03 +0100

    Added silhouette icon as notification icon on Android L and above.

--- app/src/main/java/pl/droidcon/app/reminder/ReminderReceiver.java
+++ app/src/main/java/pl/droidcon/app/reminder/ReminderReceiver.java
@@ -6,7 +6,9 @@ import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.support.annotation.DrawableRes;
 import android.support.annotation.NonNull;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.NotificationCompat;
 import android.util.Log;
 
@@ -57,7 +59,8 @@ public class ReminderReceiver extends BroadcastReceiver {
                 });
     }
 
-    private void removeNotification(final Context context, final SessionEntity sessionEntity) {
+    private void removeNotification(@NonNull final Context context,
+                                    @NonNull final SessionEntity sessionEntity) {
         DroidconInjector.get()
                 .databaseManager()
                 .removeFromNotification(sessionEntity)
@@ -73,21 +76,45 @@ public class ReminderReceiver extends BroadcastReceiver {
                 });
     }
 
-    private void showNotification(Context context, SessionEntity session) {
-        Intent sessionIntent = SessionActivity.getSessionIntent(context, session);
-        PendingIntent pendingIntent = PendingIntent.getActivity(context, session.getId(), sessionIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+    private void showNotification(@NonNull Context context, @NonNull SessionEntity session) {
+        final Intent sessionIntent = SessionActivity.getSessionIntent(context, session);
+        final PendingIntent pendingIntent = createNotificationPendingIntent(context, session, sessionIntent);
 
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(context);
+        final NotificationCompat.Builder builder = new NotificationCompat.Builder(context);
 
-        Notification notification = builder.setSmallIcon(R.mipmap.ic_launcher)
+        final Notification notification = builder
+                .setSmallIcon(getNotificationIcon())
                 .setContentTitle(context.getString(R.string.app_name))
                 .setContentText(context.getString(R.string.received_session_notification, session.getTitle()))
                 .setContentIntent(pendingIntent)
                 .setDefaults(Notification.DEFAULT_ALL)
                 .setAutoCancel(true)
+                .setColor(ContextCompat.getColor(context, R.color.primaryColorDark))
                 .build();
 
-        NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        final NotificationManager notificationManager = getNotificationManager(context);
         notificationManager.notify(session.getId(), notification);
+    }
+
+    private NotificationManager getNotificationManager(@NonNull Context context) {
+        return (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+    }
+
+    @DrawableRes
+    private int getNotificationIcon() {
+        final boolean useWhiteIcon = (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP);
+        return useWhiteIcon ? R.drawable.ic_droidcon_silhouette : R.mipmap.ic_launcher;
+    }
+
+    @NonNull
+    private PendingIntent createNotificationPendingIntent(@NonNull Context context,
+                                                          @NonNull SessionEntity session,
+                                                          @NonNull Intent sessionIntent) {
+        return PendingIntent.getActivity(
+                context,
+                session.getId(),
+                sessionIntent,
+                PendingIntent.FLAG_UPDATE_CURRENT
+        );
     }
 }
