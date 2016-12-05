481a72d94d41f39502967ed1b067433808265847
Przemek Jakubczyk <pjakubczyk@applause.com>
2016-12-05 21:03:35 +0100

    Using local assets for non session slots

--- app/src/main/java/pl/droidcon/app/ui/adapter/AgendaNonSessionLargeViewHolder.java
+++ app/src/main/java/pl/droidcon/app/ui/adapter/AgendaNonSessionLargeViewHolder.java
@@ -3,6 +3,7 @@ package pl.droidcon.app.ui.adapter;
 
 import com.squareup.picasso.Picasso;
 
+import pl.droidcon.app.R;
 import pl.droidcon.app.databinding.AgendaNonSessionLargeElementBinding;
 import pl.droidcon.app.helper.UrlHelper;
 import pl.droidcon.app.model.db.Session;
@@ -30,14 +31,32 @@ public class AgendaNonSessionLargeViewHolder extends BaseSessionViewHolder {
 
         binding.sessionTitle.setText(title);
         binding.sessionTime.setText(String.format("%s - %s", sessionRowEntity.slotStart(), sessionRowEntity.slotEnd()));
-//        binding.sessionDate.setText(DateTimePrinter.toPrintableStringWithDay(new DateTime(session.getDate())));
+
+//        String url = UrlHelper.assetUrl(sessionRowEntity.rowPicture());
+//        Picasso.with(binding.agendaLargeIcon.getContext())
+//                .load(url)
+//                .into(binding.agendaLargeIcon);
+
+
         String lowerCaseTitle = title.toLowerCase();
+        if (lowerCaseTitle.contains("registration") || lowerCaseTitle.contains("opening") ||
+                lowerCaseTitle.contains("closing") || lowerCaseTitle.contains("barcamp")) {
+            binding.agendaLargeIcon.setImageResource(R.drawable.ic_icon_droid_large);
+            return;
+        }
 
-        String url = UrlHelper.assetUrl(sessionRowEntity.rowPicture());
-        Picasso.with(binding.agendaLargeIcon.getContext())
-                .load(url)
-                .into(binding.agendaLargeIcon);
+        if (lowerCaseTitle.contains("coffe")) {
+            binding.agendaLargeIcon.setImageResource(R.drawable.ic_icon_coffee_large);
+            return;
+        }
 
+        if (lowerCaseTitle.contains("lunch")) {
+            binding.agendaLargeIcon.setImageResource(R.drawable.ic_icon_fork_large);
+            return;
+        }
+        if (lowerCaseTitle.contains("afterparty")) {
+            binding.agendaLargeIcon.setImageResource(R.drawable.ic_icon_party_large);
+        }
     }
 
 }
